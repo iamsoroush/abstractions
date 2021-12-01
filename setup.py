70ababcbf51ab880856ede1e0e597e96df77f5af
soroush <soroush.moazed@gmail.com>
2021-12-01 16:23:12 +0330

    add data_dir to config file for integration testing purposes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='abstractions-aimedic',
-    version='0.1.7',
+    version='0.1.8',
     license='MIT',
     author='Soroush Moazed',
     author_email='soroush.moazed@gmail.com',
