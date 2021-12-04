dcdb8fd3e41607789b09ee31fa0c8775f80373f2
soroush <soroush.moazed@gmail.com>
2021-12-04 16:00:14 +0330

    syntax

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         ''',
     download_url="https://github.com/iamsoroush/abstractions/archive/refs/tags/v_0.1.9.tar.gz",
     classifiers=[
-            'Development Status :: 3 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+            'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
             'Intended Audience :: Developers',      # Define that your audience are developers
             'Topic :: Software Development :: Build Tools',
             'License :: OSI Approved :: MIT License',   # Again, pick a license
