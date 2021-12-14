867e5e6fdcc6ddbb8f89566aaa099fd922ad1989
soroush <soroush.moazed@gmail.com>
2021-12-14 16:15:36 +0330

    added generic-trainer and generic-model-builder

--- src/abstractions/__init__.py
+++ src/abstractions/__init__.py
@@ -1,8 +1,8 @@
 from .base_class import BaseClass
-from .model_building import ModelBuilderBase
+from .model_building import ModelBuilderBase, GenericModelBuilderBase
 from .data_loading import DataLoaderBase
 from .evaluation import EvaluatorBase
 from .preprocessing import PreprocessorBase
 from .augmentation import AugmentorBase
-from .training import Trainer
+from .training import Trainer, GenericTrainer
 from .orchestration import Orchestrator
